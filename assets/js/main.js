2a2ecd17de0a068c61fbeb36f01576d9f62cfb76
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2023-12-09 17:30:11 -0300

    fix id and status funtion

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,18 @@ const btnAdd = document.querySelector('button');
 //ARREGLO DE OBJETOS
 const tasks = [
     {
-        id: 1,
-        name: 'wake up',
+        id: '',
+        name: 'Wake up',
         isSelected: false
     },
     {
-        id: 2,
-        name: 'take a shower',
+        id: '',
+        name: 'Take a shower',
         isSelected: false
     },
     {
-        id: 3,
-        name: 'have breakfast',
+        id: '',
+        name: 'Have breakfast',
         isSelected: false
     }
 ];
@@ -34,11 +34,19 @@ btnAdd.addEventListener('click', () =>{
     updateTaskDone()
   });
 
+  // GENERADORES DE ID
+tasks.forEach(obj => {
+      let id = Math.random().toString(36).slice(2)
+      obj.id = id
+  });
+  
+  const generateId = () => Math.random().toString(36).slice(2);
+
 //CONTADOR TAREAS
 function updateTask() {
 const taskCounter = document.querySelector('#taskCounter');
 taskCounter.textContent = `Total tareas: ${tasks.length}`
-} ;
+};
 
 //CONTADOR TAREAS REALIZADAS
 function updateTaskDone() {
@@ -47,25 +55,34 @@ const Done = tasks.filter(task => task.isSelected === true);
 taskDone.textContent = `Tareas realizadas: ${Done.length}`;
 }
 
-//CAMBIO DE ESTATUS CHECKBOX
 const changeStatus = (id) =>{
     const taskIndex = tasks.findIndex((task) => task.id === id)
-    taskIndex.isSelected = !taskIndex.isSelected
+    if (tasks[taskIndex].isSelected === false){
+        const newStatus = {
+            id: tasks[taskIndex].id,
+            name: tasks[taskIndex].name,
+            isSelected: true
+        }
+    tasks.splice(taskIndex, 1, newStatus);
+}else {
+    const newStatus = {
+        id: tasks[taskIndex].id,
+        name: tasks[taskIndex].name,
+        isSelected: false
     }
-    
-//GENERADORES DE ID
-tasks.forEach(obj => {
-    let id = Math.random().toString(36).slice(2)
-    obj.id = id
-});
+    tasks.splice(taskIndex, 1, newStatus);
+}
+render()
+updateTask();
+updateTaskDone();
 
-const generateId = () => Math.random().toString(36).slice(2);
+};
 
 //RENDERIZADOR
 const render = () => {
     taskList.innerHTML = tasks.map((task) => 
     `<div class='info'><p>${task.id}</p> <p> ${task.name}</p> 
-    <input id='check' type='checkbox' ${task.isSelected ? 'checked' :''} onclick='changeStatus(${task.id})'> </>
+    <input id='check' type='checkbox' ${task.isSelected ? 'checked' :''} onclick='changeStatus("${task.id}")'>
     <button onclick='clean("${task.id}")'>Eliminar</button>
     </div>
     
